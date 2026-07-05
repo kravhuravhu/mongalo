a1f38e894b873ae49458633de13d23aeb64358ed
Khuliso J. Ravhuravhu <khuliso.ravhuravhu@gmail.com>
2026-07-05 22:19:52 +0200

    Books index updated with show | all routes updated

--- routes/web.php
+++ routes/web.php
@@ -11,18 +11,71 @@ use App\Http\Controllers\Web\ResourceController;
 use App\Http\Controllers\Web\ContactController;
 use App\Http\Controllers\Web\InviteController;
 
-Route::get('/', [HomeController::class, 'index'])->name('home');
-Route::get('/about', [AboutController::class, 'index'])->name('about');
-Route::get('/books', [BookController::class, 'index'])->name('books.index');
-Route::get('/books/{slug}', [BookController::class, 'show'])->name('books.show');
-Route::get('/events', [EventController::class, 'index'])->name('events.index');
-Route::get('/events/{slug}', [EventController::class, 'show'])->name('events.show');
-Route::post('/events/register', [EventController::class, 'register'])->name('events.register');
-Route::get('/invite', [InviteController::class, 'index'])->name('invite');
-Route::post('/invite', [InviteController::class, 'send'])->name('invite.send');
-Route::get('/baptism', [BaptismController::class, 'index'])->name('baptism');
-Route::post('/baptism/request', [BaptismController::class, 'request'])->name('baptism.request');
-Route::get('/community', [CommunityController::class, 'index'])->name('community');
-Route::get('/resources', [ResourceController::class, 'index'])->name('resources');
-Route::get('/contact', [ContactController::class, 'index'])->name('contact');
-Route::post('/contact', [ContactController::class, 'send'])->name('contact.send');
+// Home
+Route::get('/', [
+    HomeController::class, 'index'
+])->name('home');
+
+// About
+Route::get('/about', [
+    AboutController::class, 'index'
+])->name('about');
+
+// Books
+Route::get('/books', [
+    BookController::class, 'index'
+])->name('books.index');
+
+Route::get('/books/{slug}', [
+    BookController::class, 'show'
+])->name('books.show');
+
+// Events
+Route::get('/events', [
+    EventController::class, 'index'
+])->name('events.index');
+
+Route::get('/events/{slug}', [
+    EventController::class, 'show'
+])->name('events.show');
+
+Route::post('/events/register', [
+    EventController::class, 'register'
+])->name('events.register');
+
+// Invite Arthur
+Route::get('/invite', [
+    InviteController::class, 'index'
+])->name('invite');
+
+Route::post('/invite', [
+    InviteController::class, 'send'
+])->name('invite.send');
+
+// Baptism
+Route::get('/baptism', [
+    BaptismController::class, 'index'
+])->name('baptism');
+
+Route::post('/baptism/request', [
+    BaptismController::class, 'request'
+])->name('baptism.request');
+
+// Community
+Route::get('/community', [
+    CommunityController::class, 'index'
+])->name('community');
+
+// Resources
+Route::get('/resources', [
+    ResourceController::class, 'index'
+])->name('resources');
+
+// Contact
+Route::get('/contact', [
+    ContactController::class, 'index'
+])->name('contact');
+
+Route::post('/contact', [
+    ContactController::class, 'send'
+])->name('contact.send');
